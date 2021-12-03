7036711b6e6c9e32c007b296e411960180f928ff
Michael Gonzalez <michael@Michaels-MacBook-Pro-2.local>
2021-12-02 21:11:11 -0800

    Added translation and rotation functions

---
+++ MotionProfiling/Odom.cpp
@@ -0,0 +1,157 @@
+#include "Odom.h"
+
+Odom::Odom(volatile long *_encL, volatile long *_encR, double *_outL, double *_outR, functiontype _resetEncoders) {
+    encL = _encL;
+    encR = _encR;
+    outL = _outL;
+    outR = _outR;
+    resetEncoders = _resetEncoders;
+    
+    configureWheelbase(0, 0, 0);
+    configureTranslationPID(0, 0, 0, 0);
+    configureRotationPID(0, 0, 0);
+}
+
+void Odom::configureWheelbase(double _radius, double _length, unsigned long _tpr) {
+    radius = _radius;
+    length = _length;
+    tpr = _tpr;
+}
+
+void Odom::configureTranslationPID(double kP, double kI, double kD, double _kPs) {
+    kPt = kP;
+    kIt = kI;
+    kDt = kD;
+    kPs = _kPs;
+}
+
+void Odom::configureRotationPID(double kP, double kI, double kD) {
+    kPr = kP;
+    kIr = kI;
+    kDr = kD;
+}
+
+void Odom::startTranslate(double dist) {
+    if (!rotating) {
+        targetDist = dist;
+        prevDist = 0;
+        sumDist = 0;
+        distStopCount = 0;
+        resetEncoders();
+        translating = true;
+        Serial.println("STARTED TRANSLATE");
+    }
+}
+
+void Odom::continueTranslate() {
+    if (millis() - prevT >= sampleTime) {
+        //Serial.println("CONTINUING TRANSLATE");
+        // main calculations
+        double dist = distTraveled();
+        Serial.print("dist traveled: "); Serial.println(dist);
+        double error = targetDist - distTraveled();
+        double res = kPt * error + kIt * sumDist - kDt * (dist - prevDist);
+
+        // account for non-straightness
+        double diff = diffTraveled();
+        *outL = limit(res + kPs * diff);
+        *outR = limit(res - kPs * diff);
+
+        // check for stopping
+        if (fabs(error) <= distTol) {
+            distStopCount++;
+            if (distStopCount > countTol) {
+                Serial.println("DONE TRANSLATING");
+                *outL = 0.0;
+                *outR = 0.0;
+                translating = false;
+            }
+        }
+
+        prevDist = error;
+        sumDist += error;
+        prevT = millis();
+    }
+}
+
+void Odom::startRotate(double alpha) {
+    if (!translating) {
+        targetAlpha = alpha;
+        prevAlpha = 0;
+        sumAlpha = 0;
+        alphaStopCount = 0;
+        resetEncoders();
+        rotating = false;
+    }
+}
+
+void Odom::continueRotate() {
+    if (millis() - prevT >= sampleTime) {
+        // main calculations
+        double alpha = alphaTraveled();
+        double error = targetAlpha - alphaTraveled();
+        double res = kPr * error + kIr * sumAlpha - kDt * (alpha - prevAlpha);
+
+        // account for non-straightness
+        double diff = diffTraveled();
+        *outL = limit(res + kPs * diff);
+        *outR = limit(res - kPs * diff);
+
+        // check for stopping
+        if (fabs(error) <= alphaTol) {
+            alphaStopCount++;
+            if (alphaStopCount > countTol) {
+                *outL = 0.0;
+                *outR = 0.0;
+                rotating = false;
+            }
+        }
+
+        prevAlpha = alpha;
+        sumAlpha += error;
+        prevT = millis();
+    }
+}
+
+void Odom::loop() {
+    if (translating) {
+        continueTranslate();
+    }
+    else if (rotating) {
+        continueRotate();
+    }
+}
+
+double Odom::distTraveled() {
+    double distL = radius * TWO_PI * (*encL) / tpr;
+    double distR = radius * TWO_PI * (*encR) / tpr;
+    return (distL + distR) / 2;
+}
+
+double Odom::diffTraveled() {
+    double distL = radius * TWO_PI * (*encL) / tpr;
+    double distR = radius * TWO_PI * (*encR) / tpr;
+    return distR - distL; //pos = right has traveled further, neg = left has traveled further
+}
+
+double Odom::alphaTraveled() {
+    return 2 * diffTraveled() / length;
+}
+
+double Odom::limit(double input) {
+    if (input > 100.0) {
+        return 100.0;
+    }
+    else if (input < -100.0) {
+        return -100.0;
+    }
+    return input;
+}
+
+bool Odom::isTranslating() {
+    return translating;
+}
+
+bool Odom::isRotating() {
+    return rotating;
+}
---
+++ MotionProfiling/Odom.h
@@ -0,0 +1,68 @@
+#ifndef Odom_h
+#define Odom_h
+
+#include <Arduino.h>
+#include "FunctionType.h"
+
+class Odom {
+    public:
+        Odom(volatile long *_encL, volatile long *_encR, double *_outL, double *_outR, functiontype _resetEncoders);
+        void configureWheelbase(double _radius, double _length, unsigned long _tpr);
+        void configureTranslationPID(double kP, double kI, double kD, double _kPs);
+        void configureRotationPID(double kP, double kI, double kD);
+
+        void startTranslate(double dist);
+        void startRotate(double alpha);
+
+        void loop();
+
+        bool isTranslating();
+        bool isRotating();
+
+
+    private:
+        void continueTranslate();
+        void continueRotate();
+        double distTraveled();
+        double diffTraveled();
+        double alphaTraveled();
+        double limit(double input);
+
+        unsigned long sampleTime = 20; //ms
+        double alphaTol = 0.05;
+        double distTol = 0.5;
+        unsigned long countTol = 20;
+        double radius; //mm
+        double length; //mm
+        unsigned long tpr; //ticks per rev
+
+        bool translating = false;
+        bool rotating = false;
+
+        double targetAlpha; //rad
+        double prevAlpha; //rad
+        double sumAlpha;
+        unsigned long alphaStopCount;
+        double targetDist; //mm
+        double prevDist; //mm
+        double sumDist;
+        unsigned long distStopCount;
+        unsigned long prevT;
+
+        double kPt;
+        double kIt;
+        double kDt;
+        double kPs;
+        double kPr;
+        double kIr;
+        double kDr;
+
+        volatile long *encL;
+        volatile long *encR;
+        double *outL;
+        double *outR;
+        functiontype resetEncoders;
+
+};
+
+#endif
